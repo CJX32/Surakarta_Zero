db7cfcbbb394a0cca8ec0d63cdbab6c422860b49
CJX32 <meteorskysun@outlook.com>
2019-07-02 17:22:16 +0800

    the beta version

--- Search_Engine.c
+++ Search_Engine.c
@@ -73,7 +73,7 @@ int Alpha_Beta_Multi_Thread(int depth, int minimaxplayer)
 
         int flag, eval, maxEval = -2147483640, origin;
 
-        Move_List *h = (Move_List *)malloc(sizeof(Move_List *));
+        Move_List *h = (Move_List *)malloc(sizeof(Move_List ));
         h->flag = 0;
         Move_Generate(h, who);
         pthread_t tids[h->flag];
@@ -84,23 +84,25 @@ int Alpha_Beta_Multi_Thread(int depth, int minimaxplayer)
         for (int a = 0; a < h->flag; a++)
         {
 
-            origin = chessboard[h->list[a].to.x][h->list[a].to.y];
-            chessboard[h->list[a].from.x][h->list[a].from.y] = 0;
-            chessboard[h->list[a].to.x][h->list[a].to.y] = who;
-            for (int a = 0; a < 6; a++)
+            for (int a = 0; a < h->flag; a++)
+        {
+           
+            
+            for (int d = 0; d < 6; d++)
             {
                 for (int b = 0; b < 6; b++)
                 {
-                    arg[a].chessboard[a][b] = chessboard[a][b];
+                    arg[a].chessboard[d][b] = chessboard[d][b];
                 }
             }
+              arg[a].chessboard[h->list[a].from.x][h->list[a].from.y] = 0;
+             arg[a].chessboard[h->list[a].to.x][h->list[a].to.y] = who;
             arg[a].alpha = -2147483648;
             arg[a].beta = 2147483647;
             arg[a].depth = depth - 1;
             arg[a].minimaxplayer = minimaxplayer;
             pthread_create(&tids[a], &attr, Alpha_Beta_pth, &arg[a]);
-            chessboard[h->list[a].to.x][h->list[a].to.y] = origin;
-            chessboard[h->list[a].from.x][h->list[a].from.y] = who;
+       
         }
         for (int a = 0; a < h->flag; a++)
         {
@@ -112,31 +114,23 @@ int Alpha_Beta_Multi_Thread(int depth, int minimaxplayer)
         }
         return maxEval;
     }
+    }
     else
     {
 
         int flag, eval, miniEval = 2147483640, origin;
 
-        Move_List *h = (Move_List *)malloc(sizeof(Move_List *));
+        Move_List *h = (Move_List *)malloc(sizeof(Move_List ));
         h->flag = 0;
         Move_Generate(h, -who);
         pthread_t tids[h->flag];
         Para arg[h->flag];
         pthread_attr_t attr;
         pthread_attr_init(&attr);
-        int original[6][6];
-        for (int a = 0; a < 6; a++)
-        {
-            for (int b = 0; b < 6; b++)
-            {
-                original[a][b] = chessboard[a][b];
-            }
-        }
+
         for (int a = 0; a < h->flag; a++)
         {
-            origin = chessboard[h->list[a].to.x][h->list[a].to.y];
-            chessboard[h->list[a].from.x][h->list[a].from.y] = 0;
-            chessboard[h->list[a].to.x][h->list[a].to.y] = who;
+            
             for (int d = 0; d < 6; d++)
             {
                 for (int b = 0; b < 6; b++)
@@ -144,55 +138,21 @@ int Alpha_Beta_Multi_Thread(int depth, int minimaxplayer)
                     arg[a].chessboard[d][b] = chessboard[d][b];
                 }
             }
+             arg[a].chessboard[h->list[a].from.x][h->list[a].from.y] = 0;
+             arg[a].chessboard[h->list[a].to.x][h->list[a].to.y] = -who;
             arg[a].alpha = -2147483648;
             arg[a].beta = 2147483647;
             arg[a].depth = depth - 1;
             arg[a].minimaxplayer = minimaxplayer;
             pthread_create(&tids[a], &attr, Alpha_Beta_pth, &arg[a]);
-            chessboard[h->list[a].to.x][h->list[a].to.y] = origin;
-            chessboard[h->list[a].from.x][h->list[a].from.y] = -who;
-           
+       
         }
 
         for (int a = 0; a < h->flag; a++)
         {
             pthread_join(tids[a], NULL);
         }
-        for (int a = 0; a < 6; a++)
-        {
-            for (int b = 0; b < 6; b++)
-            {
-                if (original[a][b] != chessboard[a][b])
-                {
-                    printf("depth=%d Big Error!!\n\n", depth);
-                    for (int c = 0; c < 6; c++)
-                    {
-                        for (int d = 0; d < 6; d++)
-                        {
-                            if (chessboard[c][d] == -1)
-                                printf("2 ");
-                            else
-                                printf("%d ", chessboard[c][d]);
-                        }
-                        printf("\n");
-                    }
-                    printf("\n");
-
-                    for (int c = 0; c < 6; c++)
-                    {
-                        for (int d = 0; d < 6; d++)
-                        {
-                            if (original[c][d] == -1)
-                                printf("2 ");
-                            else
-                                printf("%d ", original[c][d]);
-                        }
-                        printf("\n");
-                    }
-                    exit(0);
-                }
-            }
-        }
+      
 
         for (int a = 0; a < h->flag; a++)
         {
