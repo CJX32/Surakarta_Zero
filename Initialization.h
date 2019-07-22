97e562475c98f390d8877b2e0552c2c7b39c0d84
CJX32 <meteorskysun@outlook.com>
2019-07-22 16:36:03 +0800

    the beta version

--- Initialization.h
+++ Initialization.h
@@ -1,8 +1,8 @@
 #ifdef Initialization_h
 #define Initialization_h
 #include <stdlib.h>
-#include "Basic.h"
 #include "Hash_Table.h"
+#include "Basic.h"
 void Chessboard_Init(int chessboard[][6]);
 void Hash_Board_Init(uint64_t Hash_Board[6][6][2]);
 void Hash_Table_Init(Hash_Move *p);
