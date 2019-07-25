37687c6fad5fb5cd4a50fa23ef5c4e1dbb15a53d
CJX32 <meteorskysun@outlook.com>
2019-07-25 22:14:46 +0800

    the beta version

--- test.c
+++ test.c
@@ -81,9 +81,9 @@ void test_alpha_beta(FILE *fp){
        int alpha=-2147483648,beta=2147483647;
        for(int a=1;a<=7;a++){
            data[a-1]=Alpha_Beta_Multi_Thread(a, 1,alpha,beta);
-           haha=data[a-1];
+        /*    haha=data[a-1];
            alpha=haha-1;
-           beta=haha+1;
+           beta=haha+1;*/
        }
        for(int e=0;e<7;e++){
            if(data[e]!=index[e]){
